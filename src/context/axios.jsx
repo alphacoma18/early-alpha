8df69bb112926e8567b628d7dbba0717dcc8d521
mnhs-shs <alpha.coma.ict@gmail.com>
2022-05-05 13:28:52 +0800

    head changes

--- src/context/axios.jsx
+++ src/context/axios.jsx
@@ -4,16 +4,17 @@ export const axios = Axios.create({
 	baseURL: "https://mnhs-shs.herokuapp.com",
 	headers: {
 		"Content-Type": "application/json",
-		// "Access-Control-Allow-Origin": [
-		// 	"https://github.com/mnhs-shs/early-alpha",
-		// 	"https://mnhs-shs.herokuapp.com",
-		// ],
-		// "Access-Control-Allow-Credentials": true,
-		// "Access-Control-Allow-Origin": true,
-		// "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-		// "Access-Control-Allow-Headers": "Content-Type, Authorization",
-		// "Access-Control-Max-Age": "3600",
-		// "Access-Control-Expose-Headers":
-		// 	"Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Access-Control-Allow-Methods, Access-Control-Allow-Credentials",
+		"Access-Control-Allow-Origin": [
+			"https://github.com/mnhs-shs/early-alpha",
+			"https://mnhs-shs.herokuapp.com",
+			"https://cheerful-llama-689748.netlify.app"
+		],
+		"Access-Control-Allow-Credentials": true,
+		"Access-Control-Allow-Origin": true,
+		"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+		"Access-Control-Allow-Headers": "Content-Type, Authorization",
+		"Access-Control-Max-Age": "3600",
+		"Access-Control-Expose-Headers":
+			"Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Access-Control-Allow-Methods, Access-Control-Allow-Credentials",
 	},
 });
